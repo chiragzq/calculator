68a0e777b60a33a3002291295660fa195c305918
Mauricio Narvaez <maunvz@stanford.edu>
2016-12-04 15:45:34 -0800

    changed stuff

--- app/src/main/java/com/chirag/calculator/calculator/MainActivity.java
+++ app/src/main/java/com/chirag/calculator/calculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.chirag.calculator.calculator;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,12 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     String mEquation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
 
         mEquation = "";
 
